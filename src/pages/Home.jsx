e47ab18bdf2576c9197c0f9a97ebcfcaa32b0448
Patrick-Ngige <wakemanjajr@gmail.com>
2023-07-19 00:29:57 +0300

    creating the button in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ import {
 } from  '../config/motion';
 import state from '../store';
 import React from 'react';
+import { CustomButton } from '../components';
 
 const Home = () => {
   const snap = useSnapshot(state);
@@ -31,10 +32,21 @@ const Home = () => {
                 DO IT.
               </h1>
             </motion.div>
-            <motion.div>
-              <p>
+            <motion.div 
+            {...headContentAnimation} 
+            className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-600 text-base">
                 Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unlease your imagination</strong>{""} and define your own style.
                 </p>
+
+                <CustomButton
+                  type="filled"
+                  title="Customize It"
+                  handleClick={() => state.intro=false}
+                  CustomStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                  >
+                </CustomButton>
             </motion.div>
           </motion.div>
         </motion.section>
