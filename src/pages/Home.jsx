c9c5e418d24fe253b14f1e69ad035dd14c38e871
Patrick-Ngige <wakemanjajr@gmail.com>
2023-07-19 01:25:15 +0300

    customizing the button in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
-import { motion, AnimatePresence } from 'framer-motion';
-import { useSnapshot } from 'valtio';
-import { 
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+import {
   headContainerAnimation,
   headContentAnimation,
   headTextAnimation,
-  slideAnimation
-} from  '../config/motion';
-import state from '../store';
-import React from 'react';
-import { CustomButton } from '../components';
+  slideAnimation,
+} from "../config/motion";
+import state from "../store";
+import React from "react";
+import { CustomButton } from "../components";
 
 const Home = () => {
   const snap = useSnapshot(state);
@@ -16,43 +16,43 @@ const Home = () => {
   return (
     <AnimatePresence>
       {snap.intro && (
-        <motion.section className="home" {...slideAnimation('left')}>
+        <motion.section className="home" {...slideAnimation("left")}>
           <motion.header {...slideAnimation("down")}>
-            <img 
-            src="./threejs.png" 
-            alt="logo" 
-            className="w-8 h-8 object-contain"
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
             />
           </motion.header>
-          <motion.div
-          className="home-content" {...headContainerAnimation}>
+          <motion.div className="home-content" {...headContainerAnimation}>
             <motion.div {...headTextAnimation}>
               <h1 className="head-text">
-                LET'S <br className="xl:block hidden"/>
+                LET'S <br className="xl:block hidden" />
                 DO IT.
               </h1>
             </motion.div>
-            <motion.div 
-            {...headContentAnimation} 
-            className="flex flex-col gap-5"
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
             >
               <p className="max-w-md font-normal text-gray-600 text-base">
-                Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unlease your imagination</strong>{""} and define your own style.
-                </p>
+                Create your unique and exclusive shirt with our brand-new 3D
+                customization tool. <strong>Unlease your imagination</strong>
+                {""} and define your own style.
+              </p>
 
-                <CustomButton
-                  type="filled"
-                  title="Customize It"
-                  handleClick={() => state.intro=false}
-                  CustomStyles="w-fit px-4 py-2.5 font-bold text-sm"
-                  >
-                </CustomButton>
+              <CustomButton
+                type="filled"
+                title="Customize It"
+                handleClick={() => state.intro = false}
+                CustomStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
             </motion.div>
           </motion.div>
         </motion.section>
       )}
     </AnimatePresence>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
