183f5d7c4403d72f754d85cd8d5701dc283d47c3
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-06 01:17:05 +0300

    setting the model rotation smoothly

--- src/canvas/CameraRig.jsx
+++ src/canvas/CameraRig.jsx
@@ -1,14 +1,18 @@
 import { useFrame } from "@react-three/fiber"
 import { easing } from "maath";
 import state from '../store';
+import { useRef } from "react";
+import { useSnapshot } from "valtio";
 
 
-const CameraRig = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const CameraRig = ({children}) => {
+    const group = useRef();
+    const snap = useSnapshot(state);
+
+    //set the model rotation smoothly
+    
+
+  return <group ref={group}>{children}</group>   
 }
 
 export default CameraRig
