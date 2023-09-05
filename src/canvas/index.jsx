e77bb11a3e948452d4e7d9a3cbb5c8bfca4d76c8
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-06 00:45:30 +0300

    importing components into index.jsx of the canvas folder

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
 
-const Canvas = () => {
+const CanvasModel = () => {
   return (
     <div>
       Canvas
@@ -8,4 +9,4 @@ const Canvas = () => {
   )
 }
 
-export default Canvas
+export default CanvasModel
