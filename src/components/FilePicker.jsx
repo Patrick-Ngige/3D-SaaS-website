c04e427d57387d18b8c93358ce52bcad397c53e2
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-07 01:25:08 +0300

    file upload button

--- src/components/FilePicker.jsx
+++ src/components/FilePicker.jsx
@@ -1,6 +1,9 @@
 import CustomButton from "./CustomButton";
 
+
+
 const FilePicker = (file, setFile, readFile) => {
+  console.log("File value:", file.name);
   return (
     <div className="filepicker-container">
       <div className="flex-1 flex flex-col">
@@ -8,15 +11,18 @@ const FilePicker = (file, setFile, readFile) => {
           id="file-upload"
           type="file"
           accept="image/*"
-          onChange={(e) => setFile(e.target.files[0])}
+          onChange={(e) => {
+          setFile(e.target.files[0])
+          readFile("logo")
+          }}
         />
         <label htmlFor="file-upload" className="filepicker-label">
           Upload File
         </label>
 
-        <p className="mt-4 text-green-200 text-xs ">
-          {file === "" ? "No file selected" : file.name}  
-        </p>
+        <p className="mt-4 text-green-200 text-xs">
+        {file === "" ? "No file selected" : file.name}
+      </p>
       </div>
 
       <div className="mt-4 flex flex-wrap gap-3">
