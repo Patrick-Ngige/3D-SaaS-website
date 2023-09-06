534e1317c99280293a4114e47c9d23ac02821a9f
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-06 22:40:37 +0300

    working on the backdrop component

--- src/canvas/Backdrop.jsx
+++ src/canvas/Backdrop.jsx
@@ -4,8 +4,12 @@ import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
 
 const Backdrop = () => {
   return (
-    <AccumulativeShadows>
-      <RandomizedLight />
+    <AccumulativeShadows
+      position={[0, 0, -0.14]}
+    >
+      <RandomizedLight 
+        amount={4}
+      />
       <RandomizedLight />
     </AccumulativeShadows>
   )
