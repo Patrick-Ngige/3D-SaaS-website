9abb7f09e71485b5da0b4caaa68fea401600e158
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-07 00:28:40 +0300

    setting up the file input

--- src/components/FilePicker.jsx
+++ src/components/FilePicker.jsx
@@ -3,8 +3,13 @@ import CustomButton from "./CustomButton"
 const FilePicker = (file, setFile, readFile) => {
   return (
     <div className="filepicker-container">
-      <div className="">
-
+      <div className="flex-1 flex flex-col">
+        <input 
+          id="file-upload"
+          type="file"
+          accept="image/*"
+          onChange={(e) => setFile(e.target.files[0])}
+        />
       </div>
     </div>
   )
