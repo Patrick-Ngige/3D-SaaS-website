b37fb05952bae1e0e756f5e24de6a45c15bf12b7
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-06 23:53:15 +0300

    showing tab content depending on active tab

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -15,15 +15,22 @@ const Customizer = () => {
   const [file, setFile] = useState('');
   const [prompt, setPrompt] = useState('');
   const [generatingImg, setGeneratingImg] = useState(false);
-  const [activeEditoTab, setActiveEditorTab] = useState('');
-  const [ actFilterTab, setActiveFilterTab] = useState({
+  const [activeEditorTab, setActiveEditorTab] = useState('');
+  const [activeFilterTab, setActiveFilterTab] = useState({
     logoShirt: true,
     stylishShirt: false,
   })
 
   //show tab content depending on active tab
   const generateTabContent = () => {
-
+    switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker />
+      case "filepicker":
+        return <FilePicker />
+      case "aipicker":
+        return <AIPicker />
+    }
   }
 
   return (
@@ -40,8 +47,9 @@ const Customizer = () => {
                   <Tab 
                   key={tab.name}
                   tab={tab}
-                  handleClick={() => {}} />  
+                  handleClick={() => setActiveEditorTab(tab.name)} />  
                 ))}
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
