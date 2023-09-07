7623f68df5320aa22157b9118e462c13cdc9fbf4
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-08 00:01:33 +0300

    setting up the server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({limit: "50mb"}))
+app.use(express.json({ limit: "50mb" }));
 
-app.get('/', (res, req) => {
-    res.statusCode(200).json({message: "Hello from Patrick"})
-})
+app.get("/", (res, req) => {
+  res.status(200).json({ message: "Hello from Patrick" });
+});
 
+app.listen(8080, () => console.log("Server has startes on port 8080"));
