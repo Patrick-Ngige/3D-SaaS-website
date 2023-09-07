2b97df8c97ddc235845451b435b849de1f91cfa4
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-08 02:26:57 +0300

    done setting up the customizer though with few errors

--- server/index.js
+++ server/index.js
@@ -12,8 +12,8 @@ app.use(express.json({ limig: "50mb" }));
 
 app.use("/api/v1/dalle", dalleRoutes);
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello from Patrick" });
-});
+// app.get("/api/v1/dalle", (req, res) => {
+//   res.status(200).json({ message: "Hello from Patrick" });
+// });
 
 app.listen(8080, () => console.log("Server has started on port 8080"));
