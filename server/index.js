dbb452804b52ff07d2b77de85c2f3343acb9a694
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-08 00:10:41 +0300

    setting up the dalle.routes.js file

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
-app.get("/", (res, req) => {
+app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello from Patrick" });
 });
 
-app.listen(8080, () => console.log("Server has startes on port 8080"));
+app.listen(8080, () => console.log("Server has started on port 8080"));
